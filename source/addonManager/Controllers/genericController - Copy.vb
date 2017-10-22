e68677ef2d1b260d8732db757cc6206a7860de54
jay <jay@contensive.com>
2017-10-22 14:17:13 -0400

    updates

---
+++ source/addonManager/Controllers/genericController - Copy.vb
@@ -0,0 +1,20 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports Contensive.BaseClasses
+Imports Contensive.addonManager
+
+Namespace Contensive.addonManager
+    Public Class installController
+        Public Function installCollectionFromLibrary(collectionGuid As String, ByRef ErrorMessage As String) As Boolean
+
+
+
+        End Function
+    End Class
+End Namespace
+
