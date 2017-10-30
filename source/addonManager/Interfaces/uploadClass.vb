f37cef6c4c326098f787d8e6485356db3f2d012b
jay <jay@contensive.com>
2017-10-30 07:06:26 -0400

    runs on 4.1 and 5.0

--- source/addonManager/Interfaces/uploadClass.vb
+++ source/addonManager/Interfaces/uploadClass.vb
@@ -98,8 +98,10 @@ Namespace Contensive.Addons.AddonManager
                                     '
                                     ' -- version 5.0, separate class so this project can be built with contensive 5.0 reference, but run against contensive 4.1
                                     Dim ErrorMessage As String = ""
-                                    If (Not v5InstallController.installCollectionFromUpload(cp, rnUploadCollectionFile, ErrorMessage)) Then
-                                        form.description &= cp.Html.p("ERROR: " & ErrorMessage)
+                                    If (v5InstallController.installCollectionFromUpload(cp, rnUploadCollectionFile, ErrorMessage)) Then
+                                        form.body &= cp.Html.p("Installed collection files.")
+                                    Else
+                                        form.body &= cp.Html.p("Error installing collection files, ERROR: " & ErrorMessage)
                                     End If
                                 End If
                             End If
