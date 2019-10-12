9f5a1c69b5708a489ce00cd90da923e55ce1cb15
jay <jay@contensive.com>
2019-10-12 09:13:02 -0400

    add scripts

---
+++ scripts/Env.cmd
@@ -0,0 +1,7 @@
+
+rem all paths are relative to the git scripts folder
+
+set appName=app20190821v51
+set collectionName=Add-on Manager51
+set collectionPath=..\collections\Add-on Manager51\
+set binPath=..\source\addonManager51\bin\debug\
