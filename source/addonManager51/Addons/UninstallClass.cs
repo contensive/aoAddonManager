98f92a9bb40c4b646eadd1bdf6d4bafd193c5a89
jkidwell <jay@contensive.com>
2025-05-29 08:22:53 -0400

    cleanup install

--- source/addonManager51/Addons/UninstallClass.cs
+++ source/addonManager51/Addons/UninstallClass.cs
@@ -60,37 +60,16 @@ namespace Contensive.Addons.AddonManager51 {
         private string getUnistall() {
             string returnResult = "";
             try {
-
-                bool DisplaySystem;
-                bool DbUpToDate;
-                string GuidFieldName;
-                int AddonNavigatorID;
-                string TargetCollectionName;
-                int addonid;
-                int cnt;
-                int Ptr;
-                string BodyHTML;
-                var cs = cp.CSNew();
-                string Button;
-                string ButtonList;
-                string CollectionFilename = "";
-                var Doc = new System.Xml.XmlDocument();
-                string status = "";
-                bool collectionsToBeInstalledFromFolder;
-                int TargetCollectionID;
-                string SiteKey;
                 var form = cp.AdminUI.CreateLayoutBuilderList();
                 // 
-                SiteKey = cp.Site.GetText("sitekey", "");
+                string SiteKey = cp.Site.GetText("sitekey", "");
                 if (string.IsNullOrEmpty(SiteKey)) {
                     SiteKey = cp.Utils.CreateGuid();
                     cp.Site.SetProperty("sitekey", SiteKey);
                 }
                 // 
-                DbUpToDate = Operators.CompareString(cp.Site.GetText("buildVersion"), cp.Version, false) >= 0;
-                Button = cp.Doc.GetText(RequestNameButton);
-                collectionsToBeInstalledFromFolder = false;
-                GuidFieldName = "ccguid";
+                bool DbUpToDate = Operators.CompareString(cp.Site.GetText("buildVersion"), cp.Version, false) >= 0;
+                string Button = cp.Doc.GetText(RequestNameButton);
                 if ((Button ?? "") == ButtonCancel) {
                     // 
                     // ----- redirect back to the root
@@ -98,11 +77,7 @@ namespace Contensive.Addons.AddonManager51 {
                     cp.Response.Redirect(cp.Site.GetText("adminUrl"));
                 } else {
                     if (!cp.User.IsAdmin) {
-                        // 
-                        // ----- Put up error message
-                        // 
-                        ButtonList = ButtonCancel;
-                        BodyHTML = cp.Html.p("You must be an administrator to use this tool.");
+                        string BodyHTML = cp.Html.p("You must be an administrator to use this tool.");
                     } else {
                         // installFolder = "CollectionUpload" & cp.Utils.CreateGuid().Replace("{", "").Replace("-", "").Replace("}", "")
                         // InstallPath = cp.Site.PhysicalFilePath & installFolder & "\"
@@ -113,18 +88,20 @@ namespace Contensive.Addons.AddonManager51 {
                             // Before deleting each addon, make sure it is not in another collection
                             // ---------------------------------------------------------------------------------------------
                             // 
-                            cnt = cp.Doc.GetInteger("accnt");
+                            int cnt = cp.Doc.GetInteger("accnt");
                             if (cnt > 0) {
                                 var loopTo = cnt - 1;
+                                int Ptr;
                                 for (Ptr = 0; Ptr <= loopTo; Ptr++) {
                                     if (cp.Doc.GetBoolean("ac" + Ptr)) {
-                                        TargetCollectionID = cp.Doc.GetInteger("acID" + Ptr);
-                                        TargetCollectionName = cp.Content.GetRecordName("Add-on Collections", TargetCollectionID);
+                                        int TargetCollectionID = cp.Doc.GetInteger("acID" + Ptr);
+                                        string TargetCollectionName = cp.Content.GetRecordName("Add-on Collections", TargetCollectionID);
                                         // 
                                         // Clean up rules associating this collection to other objects
                                         // 
                                         cp.Content.Delete("Add-on Collection CDef Rules", "collectionid=" + TargetCollectionID);
                                         cp.Content.Delete("Add-on Collection Module Rules", "collectionid=" + TargetCollectionID);
+                                        var cs = cp.CSNew();
                                         // 
                                         // Delete any addons from this collection
                                         // 
@@ -133,7 +110,7 @@ namespace Contensive.Addons.AddonManager51 {
                                                 // 
                                                 // Clean up the rules that might have pointed to the addon
                                                 // 
-                                                addonid = cs.GetInteger("id");
+                                                int addonid = cs.GetInteger("id");
                                                 cp.Content.Delete("Admin Menuing", "addonid=" + addonid);
                                                 cp.Content.Delete("Shared Styles Add-on Rules", "addonid=" + addonid);
                                                 cp.Content.Delete("Add-on Scripting Module Rules", "addonid=" + addonid);
@@ -149,7 +126,7 @@ namespace Contensive.Addons.AddonManager51 {
                                         // Delete the navigator entry for the collection under 'Add-ons'
                                         // 
                                         if (TargetCollectionID > 0) {
-                                            AddonNavigatorID = 0;
+                                            int AddonNavigatorID = 0;
                                             cs.Open("Navigator Entries", "name='Manage Add-ons' and ((parentid=0)or(parentid is null))");
                                             if (cs.OK()) {
                                                 AddonNavigatorID = cs.GetInteger("ID");
@@ -193,8 +170,6 @@ namespace Contensive.Addons.AddonManager51 {
                         form.columnName = "";
                         form.columnSortable = false;
                         form.columnVisible = true;
-                        // 
-                        DisplaySystem = false;
                         var addonCollectionList = DbBaseModel.createList<AddonCollectionModel>(cp, "", "name");
                         int rowPtr = 0;
                         foreach (var item in addonCollectionList) {
@@ -211,10 +186,12 @@ namespace Contensive.Addons.AddonManager51 {
                     // Output the Add-on
                     // 
                     form.name = "Uninstall Collections";
+                    form.title = "Uninstall Collections";
                     form.description = "To remove collections, select them from the list and click the Uninstall button.";
                     if (!DbUpToDate) {
                         form.description += "<div style=\"Margin-left:50px\">Warning: The site's Database needs to be upgraded.</div>";
                     }
+                    string status = "";
                     if (!string.IsNullOrEmpty(status)) {
                         form.description += "<div style=\"Margin-left:50px\">" + status + "</div>";
                     }
